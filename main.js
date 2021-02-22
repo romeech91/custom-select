eae0680cff52fcd6f34599e50fc332f8e933fb4d
romeech91 <romontino@gmail.com>
2021-02-22 21:52:21 +0300

    last

---
+++ main.js
@@ -0,0 +1,44 @@
+class Dropdown {
+    constructor(selector, options) {
+        this.$el = document.querySelector(selector);
+        this.items = options.items;
+        this.$el.querySelector('.dropdown__label').textContent = this.items[0].label;
+        this.$el.addEventListener('click', event => {
+            if (event.target.classList.contains('dropdown__label')) {
+                if (this.$el.classList.contains('open')) {
+                    this.close();
+                } else {
+                    this.open();
+                }
+            } else if (event.target.tagName.toLowerCase() === 'li') {
+                this.select(event.target.dataset.id);
+            }
+        });
+
+        const itemsHTML = this.items.map(i => {
+           return `<li data-id="${i.id}">${i.label}</li>`;
+        }).join(" ");
+
+        this.$el.querySelector('.dropdown__menu').insertAdjacentHTML('afterbegin', itemsHTML);
+    }
+    open() {
+        this.$el.classList.add('open');
+    }
+    close() {
+        this.$el.classList.remove('open');
+    }
+    select(id) {
+        const item = this.items.find(i => i.id === id);
+        this.$el.querySelector('.dropdown__label').textContent = item.label;
+        this.close();
+    }
+}
+
+const dropdown = new Dropdown('#dropdown', {
+    items: [
+        {label: 'Moscow', id: 'mks'},
+        {label: 'London', id: 'lnd'},
+        {label: 'Saint-Petersburg', id: 'spb'},
+        {label: 'Krasnodar', id: 'krn'},
+    ]
+});
